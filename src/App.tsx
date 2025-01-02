50f153a05a76d34027f686a9ea270d705ab3d70d
Sakai-san <o_edo@hotmail.com>
2025-01-03 00:09:16 +0100

    form props

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
 import { WorkingExperiences, WorkingExperiencesTypes } from "./WorkingExperiences";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { WorkingPermit, WorkingPermitTypes } from "./WorkingPermit";
