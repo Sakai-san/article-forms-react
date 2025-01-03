364ad01a1218aa5435a31a0bc3220e1eed076bfa
Sakai-san <o_edo@hotmail.com>
2025-01-03 06:22:45 +0100

    form props

--- src/WorkingPermit/WorkingPermit.tsx
+++ src/WorkingPermit/WorkingPermit.tsx
@@ -32,8 +32,8 @@ export const WorkingPermit = () => {
                   aria-labelledby="demo-row-radio-buttons-group-label"
                   name="row-radio-buttons-group"
                 >
-                  <FormControlLabel value="yes" control={<Radio />} label="yes" />
                   <FormControlLabel value="no" control={<Radio />} label="no" />
+                  <FormControlLabel value="yes" control={<Radio />} label="yes" />
                 </RadioGroup>
                 <FormHelperText>{fieldState.error?.message ?? " "}</FormHelperText>
               </FormControl>
