7f5270d0e75ab4804b9ca14e93858a723c3400dd
Sakai-san <o_edo@hotmail.com>
2025-01-03 01:22:10 +0100

    form props

--- src/WorkingPermit/types.ts
+++ src/WorkingPermit/types.ts
@@ -2,7 +2,22 @@ import { z } from "zod";
 
 export const getSchema = () =>
   z.object({
-    hasValidWorkPermit: z.preprocess((value) => value === "yes", z.boolean()),
+    hasValidWorkPermit: z.string().refine(
+      (data) => {
+        console.log("data", data, typeof data);
+        if (data === "yes") {
+          return true;
+        } else if (data === "no") {
+          return true;
+        } else {
+          return false;
+        }
+      },
+
+      {
+        message: "Please select an option",
+      }
+    ),
   });
 
 export type ValidationSchema = z.infer<ReturnType<typeof getSchema>>;
