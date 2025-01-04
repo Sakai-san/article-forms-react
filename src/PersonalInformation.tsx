0c029f14200e8d6bc9bf44b18c8ff62d1078baa2
Sakai-san <o_edo@hotmail.com>
2025-01-05 00:35:21 +0100

    state

---
+++ src/PersonalInformation.tsx
@@ -0,0 +1,51 @@
+import { Controller, useFormContext } from "react-hook-form";
+import List from "@mui/material/List";
+import Stack from "@mui/material/Stack";
+import FormControl from "@mui/material/FormControl";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormLabel from "@mui/material/FormLabel";
+import Radio from "@mui/material/Radio";
+import FormHelperText from "@mui/material/FormHelperText";
+
+import { z } from "zod";
+import { TextField } from "@mui/material";
+
+export const personalInformationSchema = () =>
+  z.object({
+    firstname: z.string(),
+    lastname: z.string(),
+    email: z.string().email(),
+  });
+
+export type PersonalInformationSchema = z.infer<ReturnType<typeof personalInformationSchema>>;
+
+export const PersonalInformation = () => {
+  const {
+    control,
+    formState: { isSubmitting },
+  } = useFormContext<PersonalInformationSchema>();
+
+  return (
+    <List sx={{ py: 0 }}>
+      <Stack py={2} gap={1}>
+        <Controller
+          name="firstname"
+          control={control}
+          defaultValue=""
+          render={({ field, fieldState }) => (
+            <TextField
+              label="First name"
+              required
+              size="small"
+              disabled={isSubmitting}
+              helperText={fieldState.error?.message ?? " "}
+              error={Boolean(fieldState.error)}
+              {...field}
+            />
+          )}
+        />
+      </Stack>
+    </List>
+  );
+};
